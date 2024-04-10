8be285e0ea48f82f3df985cdddd6641c9d8871d9
Pranshu Arora <pranshuarora1618@gmail.com>
2024-04-11 03:03:43 +0530

    completed javascript

--- RPS/app.js
+++ RPS/app.js
@@ -1,7 +1,11 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
+const winnerdisplay = document.getElementById('winner')
 const possibleChoices = document.querySelectorAll('button')
+const userscoreDisplay = document.getElementById('user-score')
+const compscoreDisplay = document.getElementById('computer-score')
+
 
 let userChoice
 let computerChoice
@@ -14,6 +18,8 @@ function makeChoice(e) {
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
     getResult()
+    scoreboard()
+    restart()
 }
 
 function generateComputerChoice() {
@@ -31,12 +37,52 @@ function getResult() {
     }
 
     if (computerChoice === userChoice) {
-        result = 'It\'s a draw!'
+        results = 0
     } else if (winConditions[userChoice] === computerChoice) {
-        result = 'You win!'
+        results = 1
     } else {
-        result = 'You lose!'
+        results = -1
     }
-
+    displayResult()
     resultDisplay.innerHTML = result
+}
+function displayResult() {
+    if (results == 0) {
+        result = "Tie"
+    }
+    else if (results == 1) {
+        result = "You Win"
+    }
+    else {
+        result = "You Lose"
+    }
+}
+let userScore = 0
+let compScore = 0
+let target = 5
+function scoreboard() {
+    if (results == 1) {
+        userScore += 1;
+    }
+    else {
+        compScore += 1;
+    }
+    userscoreDisplay.innerHTML = userScore
+    compscoreDisplay.innerHTML = compScore
+    if (userScore == target) {
+        winner = "Congratulations! You Won"
+    }
+    else if (compScore == target) {
+        winner = "The Computer has caught up to you! Try again."
+    }
+    else {
+        winner = ""
+    }
+    winnerdisplay.innerHTML = winner
+}
+function restart() {
+    if (userScore == target || compScore == target) {
+        alert("GAME RESTARTED")
+        window.location.reload();
+    }
 }
